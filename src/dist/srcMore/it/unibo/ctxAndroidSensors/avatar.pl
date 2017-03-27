a3f271e6b52f135974585e8f5f92a988837c1ec6
Lisa <Lisa@192.168.140.1>
2017-03-27 21:44:11 +0200

    first commit

---
+++ src/dist/srcMore/it/unibo/ctxAndroidSensors/avatar.pl
@@ -0,0 +1,5 @@
+%====================================================================================
+% Context ctxAndroidSensors standalone= SYSTEM-configuration: file it.unibo.ctxAndroidSensors.avatar.pl 
+%====================================================================================
+context(ctxandroidsensors, "localhost",  "TCP", "8013" ).  		 
+%%% -------------------------------------------
