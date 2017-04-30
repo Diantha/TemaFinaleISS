7ee8c5d4be6461a3a30cafbdbc792feaf70ba4dc
Lisa <Lisa@DESKTOP-87GA0LO>
2017-04-30 18:19:17 +0200

    clean generale progetto

--- src/dist/srcMore/it/unibo/ctxAndroidSensors/avatar.pl
+++
@@ -1,5 +0,0 @@
-%====================================================================================
-% Context ctxAndroidSensors standalone= SYSTEM-configuration: file it.unibo.ctxAndroidSensors.avatar.pl 
-%====================================================================================
-context(ctxandroidsensors, "localhost",  "TCP", "8013" ).  		 
-%%% -------------------------------------------
